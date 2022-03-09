0a5944b7c693693c1fcba9c530e837a6e7aba88d
Nguyễn Công Trứ <congtru28@gmail.com>
2022-03-09 18:11:12 +0700

    Update crawler.ex

--- lib/crawler.ex
+++ lib/crawler.ex
@@ -6,26 +6,30 @@ defmodule Crawler do
       get_movie_url_by_page(1)
       |> List.flatten()
       |> get_movie_html_body()
-      |> Enum.map(fn(body) ->
+      |> Enum.map(fn body ->
         {:ok, document} = Floki.parse_document(body)
         movie_name = get_movie_title(document)
         IO.inspect("Parsing movie name #{movie_name}")
+
         %{
           title: movie_name,
           link: get_movie_link(document),
           thumnail: get_movie_thumnail(document),
           year: get_movie_year(document),
           number_of_episode: get_number_of_episode(document),
-          full_series: get_full_series_status(document),
+          full_series: get_full_series_status(document)
         }
       end)
-      {_, json_data} = JSON.Encoder.encode(%{
-        crawled_at: DateTime.to_string(DateTime.utc_now() |> DateTime.add(7 * 60 * 60 , :second)),
+
+    {_, json_data} =
+      JSON.Encoder.encode(%{
+        crawled_at: DateTime.to_string(DateTime.utc_now() |> DateTime.add(7 * 60 * 60, :second)),
         total: length(data),
         items: data
       })
-      IO.inspect("Crawled data: #{json_data}")
-      write_to_file(json_data)
+
+    IO.inspect("Crawled data: #{json_data}")
+    write_to_file(json_data)
   end
 
   @doc """
@@ -41,18 +45,19 @@ defmodule Crawler do
     #   |> Floki.find(".movie-list-index.home-v2 ul.last-film-box>li>.movie-item")
     #   |> Floki.attribute("href")
     with {:ok, document} <- Floki.parse_document(raw_data) do
-        urls = document
-          |> Floki.find(".movie-list-index.home-v2 ul.last-film-box>li>.movie-item")
-          |> Floki.attribute("href")
-        if length(urls) > 0 do
-          urls ++ get_movie_url_by_page(page_index + 1)
-        else
-          urls
-        end
-      else
-        error -> IO.inspect("Error when parsing document #{inspect(error)}")
+      urls =
+        document
+        |> Floki.find(".movie-list-index.home-v2 ul.last-film-box>li>.movie-item")
+        |> Floki.attribute("href")
+
+      if length(urls) > 0,
+        do: urls ++ get_movie_url_by_page(page_index + 1),
+        else: urls
+    else
+      error ->
+        IO.inspect("Error when parsing document #{inspect(error)}")
         []
-      end
+    end
   end
 
   @doc """
@@ -71,8 +76,8 @@ defmodule Crawler do
     - urls: Array of movie url
   """
   def get_movie_html_body(urls) do
-    #Enum.map(urls, fn(url) -> Crawler.HttpClient.get(url) end)
-    Enum.map(urls, & Crawler.HttpClient.get/1)
+    # Enum.map(urls, fn(url) -> Crawler.HttpClient.get(url) end)
+    Enum.map(urls, &Crawler.HttpClient.get/1)
   end
 
   @doc """
@@ -80,8 +85,8 @@ defmodule Crawler do
   """
   def get_movie_title(body) do
     body
-      |> Floki.find(".movie-info .movie-title .title-1")
-      |> Floki.text
+    |> Floki.find(".movie-info .movie-title .title-1")
+    |> Floki.text()
   end
 
   @doc """
@@ -92,6 +97,7 @@ defmodule Crawler do
       body
       |> Floki.find("#film-content-wrapper>#film-content")
       |> Floki.attribute("data-href")
+
     head
   end
 
@@ -100,9 +106,9 @@ defmodule Crawler do
   """
   def get_movie_thumnail(body) do
     body
-      |> Floki.find(".movie-info .movie-l-img img")
-      |> Floki.attribute("src")
-      |> List.last()
+    |> Floki.find(".movie-info .movie-l-img img")
+    |> Floki.attribute("src")
+    |> List.last()
   end
 
   @doc """
@@ -110,8 +116,8 @@ defmodule Crawler do
   """
   def get_movie_year(body) do
     body
-      |> Floki.find("[rel=tag]")
-      |> Floki.text
+    |> Floki.find("[rel=tag]")
+    |> Floki.text()
   end
 
   @doc """
@@ -122,11 +128,11 @@ defmodule Crawler do
       body
       |> get_episode_list()
 
-      if(length(episode_list) > 0) do
-        List.first(episode_list)
-      else
-        nil
-      end
+    if(length(episode_list) > 0) do
+      List.first(episode_list)
+    else
+      nil
+    end
   end
 
   @doc """
@@ -152,25 +158,28 @@ defmodule Crawler do
   def get_episode_list(body) do
     body
     |> Floki.find(".movie-info .movie-meta-info .status")
-    |> Floki.text
+    |> Floki.text()
     |> String.split([" ", ",", "/"])
-    |> Enum.map(fn(text) ->
+    |> Enum.map(fn text ->
       case Integer.parse(text) do
         {value, _} -> value
         :error -> nil
       end
     end)
-    |> Enum.filter(fn(number) -> number != nil end)
+    |> Enum.filter(fn number -> number != nil end)
   end
 
   def write_to_file(data) do
-    file_name = "./crawed_data/crawler_#{:os.system_time}.json"
+    file_name = "./crawed_data/crawler_#{:os.system_time()}.json"
+
     case File.write!(file_name, data, [:raw]) do
       :ok ->
         IO.inspect("Write data to file success: #{file_name}")
+
       {:error, message} ->
         IO.inspect("Write data to file error: #{message}")
-        _ ->
+
+      _ ->
         IO.inspect("Write data to file error")
     end
   end
