d59d475cf6290381f0bad1dee8a78be38b9a9f0b
Nguyễn Công Trứ <congtru28@gmail.com>
2022-03-09 18:07:01 +0700

    Update http_client.ex

--- lib/http_client.ex
+++ lib/http_client.ex
@@ -1,14 +1,14 @@
-defmodule Crawler.HttpClient do
-  def get(url) do
-    IO.inspect(url)
-    case HTTPoison.get(url) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        body
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        []
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect reason
-        _ -> []
+  defmodule Crawler.HttpClient do
+    def get(url) do
+      url
+      |> IO.inspect()
+      |> HTTPoison.get()
+      |> case do
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+          body
+
+        _ ->
+          []
+      end
     end
   end
-end
